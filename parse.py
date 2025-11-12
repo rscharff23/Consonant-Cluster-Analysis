88fa5d86d5e98014c0bb9840820b5e91d593d036
rsch <ryan.scharff@yahoo.com>
2025-11-12 14:31:19 -0500

    fixed js and added temp solution for ci/si/etc

--- parse.py
+++ parse.py
@@ -5,7 +5,7 @@ import csv
 sentences = [] #contains full sentences after parsing 
 clusters = {} #dict to contain each cluster and their number of occurences
 consts = ['b','c','ć','d','f','g','h','j','k','l','ł','m','n','ń','p','q','r','s','ś','t','v','w','x','z','ź','ż', ' '] 
-digraphs = ['cz','sz','rz','dż','dź','dz','ch']
+digraphs = ['cz','sz','rz','dż','dź','dz','ch','si','ci','zi']
 #TODO - adjust for special cases like digraphs, (vowel)-j, etc.
 
 #remove spaces, allowing for clusters across words
@@ -25,30 +25,36 @@ def remove_vowels(chars):
 #combine digraphs (rz,cz,sz,dż,dź,ch) to one character, to count sounds better
 def combine_digraphs(s):
     chars = list(s) #array of characters
+    chars.append('.') #fixes bounding problem of some sentences not ending with period
     i = 0
     while i < len(chars)-1:
         if chars[i] + chars[i+1] in digraphs: #check if next two letters are a digraph
-            chars[i] = chars[i]+chars[i+1] #if so, combine them into one
+            if chars[i+1] + chars[i+2] == 'zi':
+                chars[i] = 'dzi' #special case for dzi
+                chars.pop(i+2)#and remove i
+            else:           
+                chars[i] = chars[i]+chars[i+1] #if so, combine them into one
             chars.pop(i+1) #and remove next
         i += 1
     return(chars)
 
-#the following approach adds all clusters of size >=2 to the db, and within each cluster
+#the following approach adds all clusters of size >= char_min to the db, and within each cluster
 #includes any subclusters of sufficient size; eg. adds not only ftb but also ft and fb
 def process_sentence(chars):
-    chars.append(' ') #prevents index out of boundsing
+    char_min = 2 #change this to adjust minimum necessary sounds
     for i in range(len(chars)-1):
-        if chars[i] != ' ': #starting at each consonant
+        if chars[i] not in [' ', 'j','ci','si','zi','dzi']: #starting at each consonant
             count = 1 #cluster must have at least 2 sounds
             while chars[i+count] != ' ': #until we find a vowel/other symbol
                 cl = ''
                 for j in range(i,i+count+1):
                     cl += chars[j] #finally add chars back together
-                if cl not in clusters.keys(): #add new entry to dict
-                    clusters.update({cl:1})
-                else: #increase cluster counter
-                    clusters[cl] += 1
-                count += 1
+                if count+1 >= char_min: #check size of cluster and add if big enough
+                    if cl not in clusters.keys(): #add new entry to dict
+                        clusters.update({cl:1})
+                    else: #increase cluster counter
+                        clusters[cl] += 1
+                count += 1 #check next char
 
 #parse file to sentences
 data_file = open("pl_pdb-ud-dev.conllu", "r", encoding="utf-8")
