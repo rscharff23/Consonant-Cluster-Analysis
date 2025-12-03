092b9dc6289920af3cbab42693dd10614927e886
rscharff23 <scharffryan860@gmail.com>
2025-12-02 23:53:28 -0500

    first plots made

--- clusters/merge.py
+++ clusters/merge.py
@@ -11,7 +11,10 @@ new = pd.merge(df1,df2,on='cluster', how='outer').fillna(0)
 new['occurences_pl'] = new['occurences_pl'].astype(int)
 new['occurences_en'] = new['occurences_en'].astype(int)
 
-new['frequency_pl'] = round(new['occurences_pl'] / n_pl, 6)
-new['frequency_en'] = round(new['occurences_en'] / n_en, 6)
+new['frequency_pl'] = new['occurences_pl'] / n_pl
+new['frequency_en'] = new['occurences_en'] / n_en
+
+new['total_frequency'] = new['frequency_pl'] + new['frequency_en']
+new['relative_frequency'] = new['frequency_pl'] / (new['frequency_pl'] + new['frequency_en'])
 
 new.to_csv('merged_clusters.csv',float_format='%.6f',index=False)
